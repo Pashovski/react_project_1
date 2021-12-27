319660e5e34bea7bbe34dee13545857fb706a4f5
Pasha <pashdoctor@gmail.com>
2021-12-27 12:55:39 -0500

    added card data

---
+++ feedback-app/src/components/FeedBackItem.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import Card from "./shared/Card"
+
+function FeedBackItem({ item }) {
+    return (
+        <Card>
+            <div className="num-display">{item.rating}</div>
+            <div className="text-display">{item.text}</div>
+        </Card>
+    )
+}
+
+FeedBackItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
+export default FeedBackItem
