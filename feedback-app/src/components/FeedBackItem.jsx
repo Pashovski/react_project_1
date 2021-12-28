4f384d99eba512618f115b4d88986f7d9e4c1d60
Pasha <pashdoctor@gmail.com>
2021-12-28 13:04:16 -0500

    added the delete

--- feedback-app/src/components/FeedBackItem.jsx
+++ feedback-app/src/components/FeedBackItem.jsx
@@ -1,16 +1,20 @@
-import PropTypes from 'prop-types'
-import Card from "./shared/Card"
+import { FaTimes } from "react-icons/fa";
+import PropTypes from "prop-types";
+import Card from "./shared/Card";
 
-function FeedBackItem({ item }) {
-    return (
-        <Card>
-            <div className="num-display">{item.rating}</div>
-            <div className="text-display">{item.text}</div>
-        </Card>
-    )
+function FeedBackItem({ item, handleDelete }) {
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
 }
 
 FeedBackItem.propTypes = {
-    item: PropTypes.object.isRequired
-}
-export default FeedBackItem
+  item: PropTypes.object.isRequired,
+};
+export default FeedBackItem;
--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types'
-import FeedBackItem from "./FeedBackItem"
+import PropTypes from "prop-types";
+import FeedBackItem from "./FeedBackItem";
 
-function FeedbackList({feedback}) {
-    if(!feedback || feedback.length === 0) {
-        return <p>No Feedback Yet</p>
-    }
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
 
-    return (
-        <div className='feedback-list'>
-            {feedback.map((item) => (
-            <FeedBackItem key={item.id} item={item} />
-        ))}
-        </div>
-    )
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedBackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
-    )
-}
-export default FeedbackList
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+export default FeedbackList;
