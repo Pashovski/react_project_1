6d8c120389a6381712efe8d94db1e9b587095789
Pasha <pashdoctor@gmail.com>
2021-12-29 22:05:25 -0500

    added a bunnch of code

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,14 +1,22 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
 import Header from "./components/Header";
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedBackStats from './components/FeedBackStats';
+import FeedBackForm from './components/FeedBackForm';
 
 function App() {
     const [feedback, setFeedback] = useState (FeedbackData)
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
-            setFeedback()
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
@@ -16,6 +24,8 @@ function App() {
     <>
       <Header />
       <div className="container">
+          <FeedBackForm handleAdd={addFeedback}/>
+          <FeedBackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
       </div>
     </>
---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this Project</h1>
+        <p>This is a react app to leave feedback fora product or sercvice</p>
+        <p>Version 1.0.0</p>
+
+        <p>
+          <a href="/">Back To Home</a>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
